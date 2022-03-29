7fa9430bbfbc73500ad97be53a29d7debdea57e2
Rubens Soares <rubens.soares@sensedia.com>
2022-03-28 23:47:20 -0300

    3.6 - Add ID auto increment at SQL db.

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -4,22 +4,21 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "tab_cozinhas")
 @Getter
 @Setter
 @EqualsAndHashCode
 public class Cozinha {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nom_cozinha")
     @EqualsAndHashCode.Exclude
     private String nome;
 }
